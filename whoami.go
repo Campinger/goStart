b8804068b0702f441ef848e5407ba996083e14ae
Campinger <85894516+Campinger@users.noreply.github.com>
2024-05-01 11:20:47 +0300

    whoami
    Simple program, say's who am i, in this big world...

---
+++ whoami.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func whoami() {
+	// whoami?
+	var name string = "Platon"
+	age := 19
+	work := false
+	fmt.Print("Hello! I'm ")
+	fmt.Println(name)
+	fmt.Print(age)
+	fmt.Println(" Y.O")
+	fmt.Print("Working? ")
+	fmt.Println(work)
+}
